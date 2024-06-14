7e11681c7653af8ac175b10dee9bfdcb1f7806f4
destbreak <rey.hadiansyah7th@gmail.com>
2024-06-15 03:13:08 +0700

    create transaction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Paper } from "@mui/material";
+import TransactionList from "./components/TransactionList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [transactions, setTransactions] = useState([
+    {
+      id: 1,
+      date: "14/05/2024 07:09:45",
+      depoOrigin: "Gudang Farmasi",
+      depoDestination: "Farmasi Rawat Jalan",
+      description: "Pengiriman Stok",
+      totalPrice: 54240,
+    },
+    {
+      id: 2,
+      date: "14/05/2024 08:34:21",
+      depoOrigin: "Gudang Farmasi",
+      depoDestination: "Farmasi Rawat Inap",
+      description: "Pemenuhan Permintaan",
+      totalPrice: 3000000,
+    },
+    {
+      id: 3,
+      date: "14/05/2024 08:54:03",
+      depoOrigin: "Farmasi Rawat Inap",
+      depoDestination: "Farmasi IGD",
+      description: "Pinjam Obat",
+      totalPrice: 500000,
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Paper>
+      <h1>Daftar Perpindahan Obat</h1>
+      <TransactionList transactions={transactions} />
+    </Paper>
+  );
 }
 
-export default App
+export default App;
